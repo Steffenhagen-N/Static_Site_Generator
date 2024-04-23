f3ce4cec8f45851cf1facc52ca583ee96fcf56d3
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-23 15:57:16 -0700

    added LeafNode subclass of HTMLNode, and structured test framework

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -8,6 +8,22 @@ class TestHTMLNode(unittest.TestCase):
         html_node = HTMLNode("h1", "This is an HTMLNode", [], {"href": "https://www.google.com", "target": "_blank"})
         print(html_node)
         print(html_node.props_to_html())
+        print("")
+
+    def test_LeafNode_no_value(self):
+        pass
+
+    def test_LeafNode_tag_value_props(self):
+        pass
+
+    def test_LeafNode_value_props(self):
+        pass
+
+    def test_LeafNode_tag_value(self):
+        pass
+
+    def test_repr_LeafNode(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
