574ecaa6fb9f55216b475ee2c81a39657bb62ff9
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-24 18:26:54 -0700

    finished testing LeafNodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,24 +6,46 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_html(self):
         html_node = HTMLNode("h1", "This is an HTMLNode", [], {"href": "https://www.google.com", "target": "_blank"})
+        print("")
+        print("Testing HTMLNode props_to_HTML")
         print(html_node)
         print(html_node.props_to_html())
         print("")
 
     def test_LeafNode_no_value(self):
-        pass
+        leaf = LeafNode("a", None, {"target": "_blank"})
+        print("")
+        print("Testing no_value LeafNode")
+        print(leaf.to_html())
+        print("")
 
     def test_LeafNode_tag_value_props(self):
-        pass
+        leaf = LeafNode("a", "This will successfully transform to HTML", {"href": "https://www.boot.dev", "maxlength": "50%"})
+        print("")
+        print("Testing full LeafNode to_HTML")
+        print(leaf.to_html())
+        print("")
 
     def test_LeafNode_value_props(self):
-        pass
+        leaf = LeafNode(None, "This will successfully transform to HTML", {"href": "https://www.boot.dev"})
+        print("")
+        print("Testing value, props LeafNode to_HTML")
+        print(leaf.to_html())
+        print("")
 
     def test_LeafNode_tag_value(self):
-        pass
+        leaf = LeafNode("a", "This will successfully transform to HTML", None)
+        print("")
+        print("Testing tag, value LeafNode to_HTML")
+        print(leaf.to_html())
+        print("")
 
     def test_repr_LeafNode(self):
-        pass
+        leaf = LeafNode("a", "This will successfully transform to HTML", {"href": "https://www.boot.dev"})
+        print("")
+        print("Testing repr LeafNode")
+        print(leaf)
+        print("")
 
 if __name__ == "__main__":
     unittest.main()
