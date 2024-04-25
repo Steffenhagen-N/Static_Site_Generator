405037af95081a4d1cffed036d87bba0d6e23fdc
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-25 00:13:23 -0700

    implemented text_node to html, requires testing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -56,3 +56,31 @@ class ParentNode(HTMLNode):
         translate_children(self.children)
 
         return f"<{self.tag}{self.props_to_html()}>{children_string}</{self.tag}>"
+
+
+# ------------------ THIS NEEDS TO BE TESTED --------------------------
+def text_node_to_html_node(text_node):
+    valid_types = ["text", "bold", "italic", "code", "link", "image"]
+
+    if text_node.text_type not in valid_types:
+        raise ValueError("TextNode has invalid text_type")
+    
+    if text_node.text_type == "text":
+        return LeafNode(tag=None, value=text_node.text, props=None)
+    elif text_node.text_type == "bold":
+        return LeafNode(tag="b", value=text_node.text, props=None)
+    elif text_node.text_type == "italic":
+        return LeafNode(tag="i", value=text_node.text, props=None)
+    elif text_node.text_type == "code":
+        return LeafNode(tag="code", value=text_node.text, props=None)
+    elif text_node.text_type == "link":
+        if not text_node.url:
+            raise ValueError("Missing URL for link-type LeafNode")
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == "image":
+        if not text_node.url:
+            raise ValueError("Missing URL for image-type LeafNode")
+        if not text_node.text:
+            raise ValueError("Missing Alt. Text for image-type LeafNode")
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+# ------------------- THIS NEEDS TO BE TESTED ------------------------
